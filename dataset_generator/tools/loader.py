2003123de28cf396f4aade4b21f5c55f8557847f
Eirik Gribbestad Gustafsson <eirikgg94@gmail.com>
2021-03-05 20:07:39 +0100

    Improved loader and added more complex model

--- dataset_generator/tools/loader.py
+++ dataset_generator/tools/loader.py
@@ -3,7 +3,7 @@ import os, pyrender, trimesh
 def load_model(model_path):
     suffix = model_path.split('.')[-1]                      # Get model suffix
 
-    if 'obj'==suffix:
+    if suffix in ('obj', 'stl'):
         return load_obj(model_path)
 
     else:
