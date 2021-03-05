c4a74000e1ba8ef8330ad5dc898142e67d63730c
Eirik Gribbestad Gustafsson <eirikgg94@gmail.com>
2021-03-05 19:23:16 +0100

    Mask generator creates new scene to avoid rendering other objects than the main object

--- dataset_generator/scene/generators/mask_generator.py
+++ dataset_generator/scene/generators/mask_generator.py
@@ -1,13 +1,20 @@
-import pyrender, math, trimesh
+import pyrender, trimesh
 
 import numpy as np
 
 from PIL import Image
 
 def get_mask(scene, renderer, model_node):
-    '''Creates mask of model from scene.'''
-    _, depth = renderer.render(scene)                               # Get image
-    
+    '''Creates mask of model from scene by using depth information.'''
+    main_camera = scene.main_camera_node                            # Get main camera
+
+    new_scene = pyrender.Scene()                                    # Create empty scene
+
+    new_scene.add_node(main_camera)                                 # Add camera to new scene
+    new_scene.add_node(model_node)                                  # Add main model to scene
+
+    _, depth = renderer.render(new_scene)                           # Get image
+
     mask = np.where(0<depth, 1, 0)                                  # Filter values to create mask
 
     return Image.fromarray(np.uint8(mask*255), 'L')                 # Convert from 2d array to pil image
